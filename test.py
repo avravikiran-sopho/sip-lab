a02d7882a20aac8884c8a1d092ddbe9d4d6000fe
avravikiran-sopho <me15btech11039@iith.ac.in>
2017-06-13 22:30:41 +0530

    push

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.slider import Slider
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.uix.popup import Popup
 from scilab2py import scilab
 scilab.getd
 import os
@@ -12,11 +13,17 @@ import numpy as np
 
 class SimulatorApp(App):
     fnm = ''
+    #popup = Popup(content=Label(text='Hello world'))
+    #popup.bind(on_dismiss=my_callback)
+    #popup.open()
+    class CustomPopup():
+        pass
     def showfc(self,mainimg,fcw,fchooser):
         fchooser.height = fchooser.parent.height*6.5
         fcw.height = fcw.parent.height*6.5
         mainimg.source='black.png'
-
+        #popup = Popup(title='Test popup',content=Label(text='Hello world'),size_hint=(None, None), size=(400, 400))
+        #my_callback('ddddd')
     def showmainimg(self,mainimg,fcw,fchooser):
         fchooser.height = fchooser.parent.height*0
         fcw.height = fcw.parent.height*0
@@ -25,10 +32,6 @@ class SimulatorApp(App):
             self.fnm = fchooser.selection[0]
         except:
             print fchooser.selection
-
-
-
-
     def submit(self,rvalue,bvalue,gvalue,rstart,rend,cstart,cend,mainimg,img1,img2,img3,img4,img5):
         print rvalue.text
         print bvalue.text
@@ -37,16 +40,11 @@ class SimulatorApp(App):
         print rend.text
         print cstart.text
         print cend.text
-
-
-
-
         rgb = np.matrix("'"+(rvalue.text)+","+(gvalue.text)+","+(bvalue.text)+"'")
         subrow = np.matrix("'"+(rstart.text)+","+(rend.text)+"'")
         subcol = np.matrix("'"+(cstart.text)+","+(cend.text)+"'")
         outpath = os.getcwd()+"/"
         scilab.imgdisplay(self.fnm,rgb,subrow,subcol,'win4pix.txt',outpath)
-
         img1.source = 'out_subset_img.jpg'
         img2.source = 'out_original_img.jpg'
         img3.source = 'out_hist_band 1.jpg'
@@ -59,11 +57,12 @@ class SimulatorApp(App):
         img4.reload()
         img5.reload()
         mainimg.reload()
-
+    def show_popup(self):
+        p = self.CustomPopup()
+        p.open()
     def simulator(self, label):
         try:
             label.text = (eval(label.text))
         except:
             label.text = 'syn error'
-
 SimulatorApp().run()
